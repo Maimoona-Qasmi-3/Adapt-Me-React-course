79f12eb464952227a327a22d7c623a29ab2dbd37
Maimoona-Qasmi-3 <maimoonaqasmi3@gmail.com>
2023-01-05 00:16:00 +0500

    Fetching new Apis

--- src/fetchPet.jsx
+++ src/fetchPet.jsx
@@ -1,5 +1,12 @@
 const fetchPet = async ({queryKey}) => {
     const id = queryKey[1];
 
-    const apiRest = await fetch(`http://pets-v2.dev-apis.com/pets?id=`${id});
-}
+    const apiRest = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`);
+    if(!apiRest.ok){
+        throw new Error(`details/${id} fetch do not works`);
+    }
+
+    return apiRest.json();
+};
+
+export default fetchPet;
